71d29dec3581a6fa58600d697386a1d4a8ccfb3f
Tihomir Zhelyazkov <88166066+tihomirtx88@users.noreply.github.com>
2023-04-10 10:22:05 +0300

    create hoc

--- src/routes.js
+++ src/routes.js
@@ -2,14 +2,17 @@ import "./App.css";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import Home from "./components/home";
 import Header from "./components/navigation/header";
+import MainLayout from "./hoc/mainLayout";
 
 const Router = () => {
   return (
     <BrowserRouter>
-      <Header/>
-      <Routes>
-        <Route path="/" element={<Home/>} />
-      </Routes>
+      <Header />
+      <MainLayout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </MainLayout>
     </BrowserRouter>
   );
 };
